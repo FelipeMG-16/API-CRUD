5c1489a2c15b534b230dfa584935964427352c03
Felipe de Jesús Maqueda <83495992+FelipeMG-16@users.noreply.github.com>
2024-08-30 01:16:08 -0600

    feat: Se modifican clases POJO a entidades para BD

--- src/main/java/com/school/management/api_crud/entity/Alumno.java
+++ src/main/java/com/school/management/api_crud/entity/Alumno.java
@@ -2,14 +2,24 @@ package com.school.management.api_crud.entity;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
-
+@Entity
 @Data
 public class Alumno {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
+	
+	@OneToMany(mappedBy = "alumno", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Materia> materias;
 	
 }
--- src/main/java/com/school/management/api_crud/entity/Materia.java
+++ src/main/java/com/school/management/api_crud/entity/Materia.java
@@ -1,13 +1,31 @@
 package com.school.management.api_crud.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 
+@Entity
 @Data
 public class Materia {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
+	
+	
+	@ManyToOne
+	@JoinColumn(name= "alumno_id")
 	private Alumno alumno;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="profesor_id", referencedColumnName = "id")
 	private Profesor profesor;
 
 }
