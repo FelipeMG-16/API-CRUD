923adccd386de5cf559d34cf6125e02921633c39
Felipe de Jesús Maqueda <83495992+FelipeMG-16@users.noreply.github.com>
2024-08-29 20:38:49 -0600

    feat: Se agregan repositories para Alumno, Materia y Profesor

---
+++ src/main/java/com/school/management/api_crud/repository/AlumnoRepository.java
@@ -0,0 +1,9 @@
+package com.school.management.api_crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.school.management.api_crud.entity.Alumno;
+
+public interface AlumnoRepository extends JpaRepository <Long, Alumno>{
+
+}
---
+++ src/main/java/com/school/management/api_crud/repository/ProfesorRepository.java
@@ -0,0 +1,9 @@
+package com.school.management.api_crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.school.management.api_crud.entity.Profesor;
+
+public interface ProfesorRepository extends JpaRepository <Long, Profesor>{
+
+}
