ef69ede0030a7d22e1b4883420bc8d777d310bdc
Felipe de Jesús Maqueda <83495992+FelipeMG-16@users.noreply.github.com>
2024-08-29 19:13:27 -0600

    feat: Se agregan archivos base para proyecto de API CRUD

---
+++ src/main/java/com/school/management/api_crud/ApiDeGestionEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.school.management.api_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeGestionEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeGestionEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/management/api_crud/ApiDeGestionEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.management.api_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeGestionEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
